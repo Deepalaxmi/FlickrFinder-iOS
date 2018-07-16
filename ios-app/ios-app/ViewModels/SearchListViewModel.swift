59c65260f0c04545f8b03e87ef3a1426f838a7a7
Vincent Chau <vichau@ucdavis.edu>
2018-07-15 21:18:17 -0700

    fix indentation

--- ios-app/ios-app/ViewModels/SearchListViewModel.swift
+++ ios-app/ios-app/ViewModels/SearchListViewModel.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class SearchListViewModel {
-
+    
     // MARK: - Static Variables
 
     static var imageCache = NSCache<NSString, AnyObject>()
@@ -24,19 +24,19 @@ class SearchListViewModel {
 
     var loadError: Dynamic<Error?> = Dynamic(nil)
 
-	// MARK: - Public Variables
+    // MARK: - Public Variables
 
-	var canLoadMore: Bool = true
+    var canLoadMore: Bool = true
 
     // MARK: - Property Observers
 
     var searchTerm: String = "" {
         didSet {
-			didUpdateSearchTerm()
+            didUpdateSearchTerm()
         }
     }
 
-	var viewModels: [SearchResultViewModel]? = nil {
+    var viewModels: [SearchResultViewModel]? = nil {
         didSet {
             needsRefresh.value = true
         }
@@ -63,16 +63,16 @@ class SearchListViewModel {
 
     // MARK: - Helper Methods
 
-	func didUpdateSearchTerm() {
-		guard searchTerm != "" else { return }
-		throttler.throttle { [unowned self] in
-			self.loadSearchResults(with: self.searchTerm, clearResults: true)
-		}
-	}
+    func didUpdateSearchTerm() {
+        guard searchTerm != "" else { return }
+        throttler.throttle { [unowned self] in
+            self.loadSearchResults(with: self.searchTerm, clearResults: true)
+        }
+    }
 
     func loadMoreResults() {
-		guard canLoadMore else { return }
-		loadSearchResults(with: searchTerm, clearResults: false)
+        guard canLoadMore else { return }
+        loadSearchResults(with: searchTerm, clearResults: false)
     }
 
 }
@@ -83,34 +83,34 @@ extension SearchListViewModel {
 
     /**
      This method loads the search results and populate the child view models. The requests are throttled to prevent excess requests.
-
+     
      - parameter query: the search term
      - parameter completion: returns a closure with error and 
      */
-	func loadSearchResults(with query: String, enableThrottling: Bool = false, clearResults: Bool = false) {
-		let currentPage = searchGroup?.page ?? 0
-		let nextPage = currentPage + 1
-		webService.loadSearchResultsServer(searchTerm: query, page: nextPage) { [weak self] error, searchGroup in
-			guard let self_ = self else { return }
-			guard error == nil else {
-				self_.loadError.value = error
-				return
-			}
-			if let searchGroup = searchGroup as? SearchGroup, let searchResults = searchGroup.searchResults {
-				var viewModels = [SearchResultViewModel]()
-				for searchResult in searchResults {
-					let viewModel = SearchResultViewModel(searchResult: searchResult)
-					viewModels.append(viewModel)
-				}
-				if clearResults {
-					self_.viewModels = viewModels
-				} else {
-					self_.viewModels?.append(contentsOf: viewModels)
-				}
-				self_.searchGroup = searchGroup
-			} else {
-				self_.canLoadMore = false // Loaded last page
-			}
-		}
-	}
+    func loadSearchResults(with query: String, enableThrottling: Bool = false, clearResults: Bool = false) {
+        let currentPage = searchGroup?.page ?? 0
+        let nextPage = currentPage + 1
+        webService.loadSearchResultsServer(searchTerm: query, page: nextPage) { [weak self] error, searchGroup in
+            guard let self_ = self else { return }
+            guard error == nil else {
+                self_.loadError.value = error
+                return
+            }
+            if let searchGroup = searchGroup as? SearchGroup, let searchResults = searchGroup.searchResults {
+                var viewModels = [SearchResultViewModel]()
+                for searchResult in searchResults {
+                    let viewModel = SearchResultViewModel(searchResult: searchResult)
+                    viewModels.append(viewModel)
+                }
+                if clearResults {
+                    self_.viewModels = viewModels
+                } else {
+                    self_.viewModels?.append(contentsOf: viewModels)
+                }
+                self_.searchGroup = searchGroup
+            } else {
+                self_.canLoadMore = false // Loaded last page
+            }
+        }
+    }
 }
